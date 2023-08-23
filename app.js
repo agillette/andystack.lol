73f8365249d4c0e8f53b084a6995c6adbb4a5130
Andy Gillette <andy.gillette@gmail.com>
2023-08-23 17:52:07 +0000

    add database logging

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.get('/', function(req, res) {
 server.on('request', app);
 server.listen(3000, function() { console.log('Listening on 3000'); });
 
+process.on('SIGINT', () => {
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	});
+});
+
 /** Websockets **/
 const WebSocketServer = require('ws').Server;
 
@@ -24,6 +33,10 @@ wss.on('connection', function connection(ws) {
 		ws.send('Welcome to my server');
 	}
 
+	db.run(`INSERT INTO visitors (count, time)
+		VALUES(${numClients} datetime('now'))
+	`);
+
 	ws.on('close', function close() {
 		console.log('A clent has disconnected');
 	});
@@ -33,5 +46,28 @@ wss.broadcast = function broadcast(data) {
 	wss.clients.forEach(function each(client) {
 		client.send(data);
 	});
+};
+
+/** Database **/
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`)
+});
+
+function getCounts() {
+	db.each('SELECT * FROM visitors', (err, row) => { console.log(row) });
+};
+
+function shutdownDB() {
+	getCounts();
+	console.log('shutting down db');
+	db.close();
 };
 
